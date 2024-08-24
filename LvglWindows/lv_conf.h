f048aa670180c9d0f59010f3fc35420d22b03c73
ataweg <werneraxel@gmx.de>
2024-08-24 09:01:39 +0300

    update lvgl (https://github.com/ataweg/lvgl.git) synchronize LVGL related submodules

--- LvglWindows/lv_conf.h
+++ LvglWindows/lv_conf.h
@@ -607,7 +607,7 @@
 #endif
 
 /*API for CreateFile, ReadFile, etc*/
-#define LV_USE_FS_WIN32 1
+#define LV_USE_FS_WIN32 0
 #if LV_USE_FS_WIN32
     #define LV_FS_WIN32_LETTER '\0'     /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
     #define LV_FS_WIN32_PATH ""         /*Set the working directory. File/directory paths will be appended to it.*/
